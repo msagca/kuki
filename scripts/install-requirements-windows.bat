424ab277d17cf1a8c65d8343df6b632e97a1710b
msagca <51697294+msagca@users.noreply.github.com>
2024-09-11 11:54:17 +0300

    fix install path of visual studio build tools

--- scripts/install-requirements-windows.bat
+++ scripts/install-requirements-windows.bat
@@ -3,8 +3,8 @@ echo Starting download of Visual Studio Build Tools...
 powershell -Command "Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vs_buildtools.exe -OutFile vs_buildtools.exe"
 echo Download complete.
 echo Starting installation of Visual Studio Build Tools...
-powershell -Command "Start-Process -Wait -FilePath .\vs_buildtools.exe -ArgumentList '--quiet --wait --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --includeOptional'"
+powershell -Command "Start-Process -Wait -FilePath .\vs_buildtools.exe -ArgumentList '--quiet --wait --norestart --nocache --installPath %RUNNER_WORKSPACE%\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --includeOptional'"
 echo Installation complete.
 echo Setting up Visual Studio environment...
-call "C:\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "%RUNNER_WORKSPACE%\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 echo Environment setup complete.
